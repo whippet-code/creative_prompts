9292828a48091a491376e41a16417f27049967c8
M. Ivkovic <m.ivkovic.dev@gmail.com>
2023-03-17 12:08:12 +0000

    base prompts route added

--- photomooch/app.js
+++ photomooch/app.js
@@ -8,6 +8,7 @@ var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const promptsRouter = require("./routes/prompts");
 
 var app = express();
 // set up PORT
@@ -22,6 +23,7 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/prompts", promptsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
