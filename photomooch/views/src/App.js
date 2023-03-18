1fa985fec8bd87fd3154db0c62e155d38234786d
M. Ivkovic <m.ivkovic.dev@gmail.com>
2023-03-18 16:10:24 +0000

    base - data from DB to render in react.

--- photomooch/views/src/App.js
+++ photomooch/views/src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+
+//import components
+import Prompt from "./components/Prompt";
 
 function App() {
+  const [prompts, setPrompts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/prompts")
+      .then((res) => res.json())
+      .then((data) => setPrompts(data));
+  }, []);
+
   return (
     <div className="App">
       <h1>Photomooch</h1>
+      {prompts.map((prompt) => (
+        <Prompt key={new Date()} {...prompt} />
+      ))}
     </div>
   );
 }
---
+++ photomooch/views/src/components/Prompt.js
@@ -0,0 +1,9 @@
+const Prompt = (props) => {
+  return (
+    <div>
+      <h1>{props.prompt}</h1>
+    </div>
+  );
+};
+
+export default Prompt;
