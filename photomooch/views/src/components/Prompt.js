3be17436e083da049132eb70cbc6ae024cd8edb3
M. Ivkovic <m.ivkovic.dev@gmail.com>
2023-03-19 07:03:18 +0000

    Prompt comp

--- photomooch/views/src/components/Prompt.js
+++ photomooch/views/src/components/Prompt.js
@@ -1,7 +1,15 @@
 const Prompt = (props) => {
   return (
-    <div>
-      <h1>{props.prompt}</h1>
+    <div className="Prompt">
+      <div className="card-holder">
+        <div className="card-front">
+          <h1 className="card-category">{props.category}</h1>
+        </div>
+        <div className="card-back">
+          <h2 className="card-title">{props.title}</h2>
+          <h3 className="card-prompt">{props.description}</h3>
+        </div>
+      </div>
     </div>
   );
 };
