14cfed8d28115fd664735b521d1a47e85d7155c5
M. Ivkovic <m.ivkovic.dev@gmail.com>
2023-04-16 16:31:31 +0100

    fixing static serve issue

--- photomooch/routes/index.js
+++ photomooch/routes/index.js
@@ -1,9 +1,6 @@
 var express = require("express");
 var router = express.Router();
 
-// Serve static files from the public directory
-app.use(express.static("public"));
-
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
